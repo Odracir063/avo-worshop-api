40b0982ec7cd2b8ae5c1fd5d672a2afac4add662
Ricardo Ruiz <ruizmunricardo@gmail.com>
2022-03-05 19:43:32 +0100

    Arreglando los request de la api para que se vean bonitos

--- src/index.js
+++ src/index.js
@@ -7,6 +7,23 @@ const url = "https://platzi-avo.vercel.app/api/avo";
 const BaseUrl = "https://platzi-avo.vercel.app";
 
 const appNode = document.querySelector('#app');
+appNode.className = "grid grid-cols-2";
+
+
+const formatPrice = (price) => {
+
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: "currency",
+        currency: "USD",
+    }).format(price);
+
+    return newPrice;
+}
+
+//Intl
+//1 - format fechas
+//2 - format monedas
+
 
 //web api
 // Conectarnos al servidor
@@ -23,19 +40,35 @@ window.fetch(url)
         //crear imagen
         const image = document.createElement('img');
         image.src = `${BaseUrl}${item.image}`;  // URL de la imagen
-
+        image.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+        
 
         //crear el titulo
         const title = document.createElement('h2');
         title.textContent = item.name;
-        title.className = 'text-xl text-green-600';
+        title.className = "text-lg text-green-600";
 
         //crear el precio
         const price = document.createElement('div');
-        price.textContent = item.price;
+        price.className = "text-gray-600";
+        price.textContent = formatPrice(item.price);
+
+
+        //metemos el precio y el titulo en un div
+        const priceAndTitle = document.createElement("div")
+        priceAndTitle.className = "text-center md:text-left";
+        priceAndTitle.appendChild(title);
+        priceAndTitle.appendChild(price);
+
+        // creamos un div que contenga todo
+        const card = document.createElement("div");
+        card.className = "md:flex bg-white rounded-lg p-6 hover:bg-green-300";
+        card.append(image, priceAndTitle);
 
         const container = document.createElement('div');
-        container.append(image, title, price);
+        container.append(card);
+        
+
         
         allItems.push(container);
     });
